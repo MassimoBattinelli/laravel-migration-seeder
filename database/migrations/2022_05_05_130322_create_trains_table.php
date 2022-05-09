acf52d4405149792b1bb4f351dfbda6171236074
massi <massimo.battinelli@gmail.com>
2022-05-09 13:05:19 +0200

    first commit

---
+++ database/migrations/2022_05_05_130322_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda');
+            $table->string('Partenza');
+            $table->string('Arrivo');
+            $table->time('Orario_partenza');
+            $table->time('Orario_arrivo');
+            $table->string('Numero_treno');
+            $table->integer('Numero_carrozze');
+            $table->boolean('Ritardo');
+            $table->boolean('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
